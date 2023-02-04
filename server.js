a85dfa6b329f6325b30be00a58b23e7c60f42ac7
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-04 19:46:33 +0530

    Assignment 1 finished

--- server.js
+++ server.js
@@ -1,40 +1,39 @@
 const http = require('http');
-const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-	const url = req.url;
-	const method = req.method;
-
-	if (url === '/') {
-		res.write('<html>');
-		res.write('<head><title>Enter message</title></head>');
-		res.write("<body><form action='/message' method='POST'><input type='text' name='name'><button type='submit'>Send</button></form></body>");
-		res.write('</html>');
-		return res.end();
-	}
-
-	if (url === '/message' && method === 'POST') {
-		const body = [];
-		req.on('data', (chunk) => {
-			console.log("chunk::",chunk)
-			body.push(chunk)
-		});
-		return req.on('end', () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split('=')[1];
-			fs.writeFileSync('message.txt',message);
-			res.statusCode = 302;
-			res.setHeader('Location','/')
-			return res.end();
-		})
-	}
-	
-	res.setHeader('Content-type', 'text/html');
-	res.write('<html>');
-	res.write('<head><title>My First Page</title></head>');
-	res.write('<body><h1>Hello Nodejs Server</h1></body>');
-	res.write('</html>');
-	res.end();
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Assignment 1</title></head>');
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/users') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Assignment 1</title></head>');
+    res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  // Send a HTML response with some "Page not found text
+  if (url === '/create-user') {
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody.split('=')[1]); // username=whatever-the-user-entered
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    res.end();
+  }
 });
 
-server.listen(2090);
+server.listen(3000);
