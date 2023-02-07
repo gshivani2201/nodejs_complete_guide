bf7ed6c7ec35b193478f5c7cb0bac6a461361d53
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-07 16:21:28 +0530

    express middleware

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const http = require('http');
+// 3rd party package
+const express = require("express");
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); //Allows the request to continue to the next middleware in line
+});
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  //...send a response here if not calling another middleware through next();
+  res.send("<h1>Hello from Express!</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
