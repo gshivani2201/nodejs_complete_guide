601cfdaf6799893397edeaf39806f5c08ec3ce03
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-08 23:11:13 +0530

    parsing incoming requests

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 // 3rd party package
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs");
-  next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the middleware");
-  res.send("<h1>Add product page</h1>");
+  res.send(
+    "<html><body><form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form></body></html>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In another middleware");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
 
