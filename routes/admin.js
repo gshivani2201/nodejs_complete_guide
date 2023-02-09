85d9a89942a78a807b4accce5c51fc928a0a48cb
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-09 14:16:46 +0530

    filtering paths and handling 404

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
+//same routes can be used for different methods like get, post, put etc.
 
+//  /add-product => for GET request
 router.get("/add-product", (req, res, next) => {
   res.send(
-    "<html><body><form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form></body></html>"
+    "<html><body><form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form></body></html>"
   );
 });
 
-router.post("/product", (req, res, next) => {
+//  /add-product => for POST request
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
--- server.js
+++ server.js
@@ -11,7 +11,12 @@ app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+//filtering paths with /admin
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
 
 app.listen(3000);
