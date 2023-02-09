e414d8deed3ed5986ed2acd3003537f6ed3974e8
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-09 14:06:17 +0530

    using express Router

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 // 3rd party package
 const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
 
 const app = express();
 
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<html><body><form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form></body></html>"
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
