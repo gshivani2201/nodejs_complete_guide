20f619690dee99bcbd8c976a7d5d159aa1312a9b
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-10 13:29:03 +0530

    styling and serving files statically

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 //filtering paths with /admin
 app.use("/admin", adminRoutes);
