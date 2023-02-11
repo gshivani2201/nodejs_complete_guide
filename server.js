25673a65d0d6a4ba8023b6ce41d889c4bfe34bf9
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-11 22:23:25 +0530

    working with EJS

--- server.js
+++ server.js
@@ -2,22 +2,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const { engine } = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  engine({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.get("/favicon.ico", (req, res) => res.sendStatus(204));
