f33332ee5dfe4a94bef4c2855c364019710578a2
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-24 09:26:35 +0530

    rendering product detail view, passing data with post req, adding a cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -13,9 +14,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, (product) => {
-    console.log(product);
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
   });
-  res.redirect("/");
 };
 
 exports.getIndex = (req, res, next) => {
@@ -35,6 +39,14 @@ exports.getCart = (req, res, next) => {
       path: "/cart",
     });
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId
+  Product.findById(prodId, (product => {
+    Cart.addProduct(prodId, product.price);
+  }))
+  res.redirect('/cart')
 };
 
 exports.getCheckout = (req, res, next) => {
