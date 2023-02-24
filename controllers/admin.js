ba55a8de01c5c2a2f4cdde714ff2b3934558845e
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-24 10:21:14 +0530

    pre-populating edit product page and linking to edit button

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,14 +4,15 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product page",
     path: "/admin/add-product",
+    editing: false,
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title
-  const imageUrl = req.body.imageUrl
-  const price = req.body.price
-  const description = req.body.description
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
@@ -28,13 +29,20 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.editProduct = (req, res, next) => {
-  const editMode = req.query.edit
+  const editMode = req.query.edit;
   if (!editMode) {
-    return res.redirect('/')
+    return res.redirect("/");
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product page",
-    path: "/admin/edit-product",
-    editing: editMode
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product page",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
   });
 };
