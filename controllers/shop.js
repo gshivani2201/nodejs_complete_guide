4ae8aef0c66ba278fdb39633322ea1a5a7c85579
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-02-28 08:43:12 +0530

    displaying cart items on cart page & deleting cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,21 +33,44 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/cart", {
-      pageTitle: "Cart",
-      path: "/cart",
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (let product of products) {
+        const cartProductsData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductsData) {
+          cartProducts.push({
+            productsData: product,
+            qty: cartProductsData.qty,
+          });
+        }
+      }
+      res.render("shop/cart", {
+        pageTitle: "Cart",
+        path: "/cart",
+        products: cartProducts
+      });
     });
   });
 };
 
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId
-  Product.findById(prodId, (product => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
     Cart.addProduct(prodId, product.price);
-  }))
-  res.redirect('/cart')
+  });
+  res.redirect("/cart");
 };
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price)
+    res.redirect("/cart");
+  })
+}
 
 exports.getCheckout = (req, res, next) => {
   Product.fetchAll((products) => {
