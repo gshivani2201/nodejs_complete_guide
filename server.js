799dd17385176e38fb7c966f2729cf13c63a60e6
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-03-09 23:22:56 +0530

    creating user model & relational tables - user & products

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path");
 
 const errorController = require("./controllers/error");
 const sequelize = require("./utils/database");
+const Product = require('./models/product');
+const User = require('./models/user')
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -24,8 +26,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then((result) => {
     // console.log(result);
     app.listen(3000);
