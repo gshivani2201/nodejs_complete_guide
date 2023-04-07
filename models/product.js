fd68e6b7a042adb81fa1b4188d682605c2b9f39b
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-04-07 11:09:53 +0530

    simplified code

--- models/product.js
+++ models/product.js
@@ -6,7 +6,7 @@ class Product {
     this.price = price;
     this.description = desc;
     this.imageUrl = imageUrl;
-    this._id = id;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
@@ -16,7 +16,7 @@ class Product {
       // update the existing product
       dbOp = db
         .collection("products")
-        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+        .updateOne({ _id: this._id }, { $set: this });
     } else {
       // save new product
       dbOp = db.collection("products").insertOne(this);
