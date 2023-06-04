857dbda355e4173235d370fba204a296b9e874a2
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-06-04 15:13:12 +0530

    adding client side js code & sending & handling background requests

---
+++ public/js/admin.js
@@ -0,0 +1,13 @@
+const deleteProduct = (btn) => {
+  const productId = btn.parentNode.querySelector("[name=productId]").value;
+  const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
+
+  fetch("/admin/product/" + productId, {
+    method: "DELETE",
+    headers: {
+      "csrf-token": csrf,
+    },
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
