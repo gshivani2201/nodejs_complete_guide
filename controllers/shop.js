5208b1142d73146fe3663d58344e3044c5d50eb0
Shivani Gupta <gupta.shivani7898@gmail.com>
2023-06-04 17:49:39 +0530

    using stripe

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const PDFDocument = require("pdfkit");
+const stripe = require("stripe")(
+  "sk_test_51NFEbbSJxOm9ftFANJw9SioSeeDOVQaH7nEVfc4UzkLEPrimijCkvjHzg1qQ1VBGHiFMfP21xAwTmgIEbdxfeV0e00C3dsmwvh"
+);
 
 const Product = require("../models/product");
 const Order = require("../models/order");
@@ -243,20 +246,79 @@ exports.getInvoice = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
   req.user
     .populate("cart.items.productId")
     .then((user) => {
-      const products = user.cart.items;
-      let total = 0;
+      products = user.cart.items;
+      total = 0;
       products.forEach((p) => {
         total += p.quantity * p.productId.price;
       });
+ 
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        line_items: products.map((p) => {
+          return {
+            quantity: p.quantity,
+            price_data: {
+              currency: "INR",
+              unit_amount: p.productId.price * 100,
+              product_data: {
+                name: p.productId.title,
+                description: p.productId.description,
+              },
+            },
+          };
+        }),
+        customer_email: req.user.email,
+        success_url:
+          req.protocol + "://" + req.get("host") + "/checkout/success",
+        cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancel",
+      });
+    })
+    .then((session) => {
       res.render("shop/checkout", {
         pageTitle: "Checkout",
         path: "/checkout",
         products: products,
         totalSum: total,
+        sessionId: session.id,
       });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((item) => {
+        return {
+          quantity: item.quantity,
+          product: { ...item.productId._doc },
+        };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user._id,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then((result) => {
+      res.redirect("/orders");
     })
     .catch((err) => {
       const error = new Error(err);
--- server.js
+++ server.js
@@ -103,6 +103,7 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
+  // console.log(error)
   res.status(500).render("500", {
     pageTitle: "Something went wrong.",
     path: "/500",
